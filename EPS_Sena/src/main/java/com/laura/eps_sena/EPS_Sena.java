23d20c07f5a62f8a6e0b2e9e3aaf50529dcdf0d1
davidjose47sudo <78758821+davidjose47sudo@users.noreply.github.com>
2023-05-04 13:10:22 -0500

    Base

---
+++ EPS_Sena/src/main/java/com/laura/eps_sena/EPS_Sena.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.laura.eps_sena;
+
+/**
+ *
+ * @author david
+ */
+public class EPS_Sena {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
