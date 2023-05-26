dfdd75ea744eabbe964a6a8c05fa2b583eca2e34
Y3ipii <68871675+Y3ipii@users.noreply.github.com>
2023-05-26 14:47:26 -0500

    Add files via upload
    El archivo "Conexion.java" contiene una clase Java llamada "Conexion" que se utiliza para establecer una conexión con una base de datos MySQL y realizar operaciones en ella. El código utiliza el controlador JDBC de MySQL para interactuar con la base de datos.

--- Conexion.java
+++ Conexion.java
@@ -1,26 +1,20 @@
+
+import com.mysql.cj.protocol.Resultset;
+import com.mysql.cj.protocol.ResultsetRow;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import javax.swing.JOptionPane;
-import javax.swing.SpringLayout;
-
-
-
-
-    
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-public class  Conexion {
-    
+public class Conexion {
+
     public static final String URL = "jdbc:mysql://localhost:3306/gestionmedica";
     public static final String USERNAME = "root";
     public static final String PASSWORD = "";
-    public static void main(String[] args) {
 
-
+    public static void conectart() {
         try {
             Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             // Aquí puedes realizar operaciones en la base de datos utilizando la conexión establecida
@@ -30,8 +24,34 @@ public class  Conexion {
             e.printStackTrace();
         }
     }
+
+    public static void paciente() {
+
+        try ( Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+            PreparedStatement req = connection.prepareStatement(URL);
+            ResultSet response = req.executeQuery("SELECT * FROM PACIENTES");
+            while (response.next()) {
+
+                int data = response.getRow();
+                System.out.println(data);
+                System.out.println(response.getString(1));
+                System.out.println(response.getString(2));
+                System.out.println(response.getString(3));
+                System.out.println(response.getString(4));
+            }
+
+            System.out.println("no hay mas datos");
+
+            // Cierra la conexión cuando hayas terminado
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        paciente();
+    }
+
 }
-
-    
-
-
